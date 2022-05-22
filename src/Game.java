02d684fe47b8347fa461fcccc8aa8fee7fbba9f5
yux3ll <yuksel.baypinar@tedu.edu.tr>
2022-05-22 17:30:43 +0300

    some edits

--- src/Game.java
+++ src/Game.java
@@ -11,7 +11,7 @@ public class Game extends Map{
 
     public void displayGraphics() {
 
-        //insert method here to display(and refresh) all graphics here(display map, blocked tiles, cat)
+        //insert method here to display(and refresh) all graphics(display map, blocked tiles, cat)
 
     }
 
